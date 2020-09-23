148c64e401cb91d20c2e368bb54c79e8030228c6
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2020-09-22 20:49:12 -0400

    Jonathan-Portillo

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
+import { connect, useDispatch } from "react-redux";
+import { addFeature } from "../actions/featureActions";
 
-const AdditionalFeature = props => {
+const AdditionalFeature = (props) => {
+  const dispatch = useDispatch();
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button
+        onClick={() => dispatch(addFeature(props.feature))}
+        className="button"
+      >
+        Add
+      </button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+const mapStateToProps = (state) => {
+  return {
+    additionalFeatures: state.additionalFeatures,
+  };
+};
+
+export default connect(mapStateToProps, { addFeature })(AdditionalFeature);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { featureReducer } from "./reducers/featureReducers";
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+const store = createStore(featureReducer);
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
